6a07cc691ccbd13f5705f0df71adbe7007da6748
Moritz John <moritz.john@posteo.com>
2026-03-25 18:15:22 +0100

    Update version to v26.03.25

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     fullname = "Heartbeat",
     description =
     "This plugin allows KOReader to send its current state (+ book info & battery level) to a Home Assistant binary sensor.",
-    version = "v26.03.19",
+    version = "v26.03.25",
 }
